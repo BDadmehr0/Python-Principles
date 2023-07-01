5d14e3fcf697b98d307ab25de1969b95d978868f
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-07-01 12:35:29 +0330

    coomit

---
+++ Min-maxing/main.py
@@ -0,0 +1,9 @@
+def largest_difference(numbers):
+    smallest = min(numbers)
+    largest = max(numbers)
+    return largest - smallest
+
+# Example usage:
+result = largest_difference([1, 2, 3])
+print(result)
+
---
+++ Writing-short-code/main.py
@@ -0,0 +1,3 @@
+def convert(numbers):
+    return list(map(str, numbers))
+
